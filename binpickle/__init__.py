a6c53817a57644631e2d1cb913bc9013b60f2461
Michael Ekstrand <michaelekstrand@boisestate.edu>
2020-06-04 10:05:42 -0600

    Bump version to 0.3.1

--- binpickle/__init__.py
+++ binpickle/__init__.py
@@ -2,7 +2,7 @@
 Optimized format for pickling binary data.
 """
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 from .write import dump, BinPickler    # noqa: F401
 from .read import load, BinPickleFile  # noqa: F401
