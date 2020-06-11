dd11695e7712e214633173d16b3e9a40dc38784c
Michael Ekstrand <md@ekstrandom.net>
2020-06-11 15:52:39 -0600

    Add repickl command for testing

---
+++ binpickle/repickle.py
@@ -0,0 +1,79 @@
+"""
+Re-pickle a file.
+
+Usage:
+    repickle [options] SRC DST
+
+Options:
+    -f FORMAT
+        The source format [default: pickle].
+    -t FORMAT
+        The destination format [default: binpickle].
+    -p PROTOCOL
+        Protocol version number.
+    -v, --verbose
+        Output debug logging.
+    SRC
+        The source file.
+    DST
+        The destination file.
+"""
+
+import warnings
+import pathlib
+import logging
+
+from natural.size import binarysize
+from docopt import docopt
+
+import binpickle
+import pickle
+if pickle.HIGHEST_PROTOCOL < 5:
+    try:
+        import pickle5 as pickle
+    except ImportError:
+        warnings.warn('No pickle5 module, only protocol 4 supported', ImportWarning)
+
+_log = logging.getLogger('binpickle.repickle')
+
+
+def read_object(src, format):
+    src = pathlib.Path(src)
+    stat = src.stat()
+    _log.info('reading %s file %s', format, src)
+    _log.info('input size %s', binarysize(stat.st_size))
+    if format == 'pickle':
+        with src.open('rb') as f:
+            return pickle.load(f)
+    elif format == 'binpickle':
+        with binpickle.BinPickleFile(src) as bpk:
+            return bpk.load()
+    else:
+        _log.error('invalid source format %s', format)
+        raise ValueError('invalid source format ' + format)
+
+
+def write_object(obj, dst, format, protocol):
+    if protocol is not None:
+        protocol = int(protocol)
+    dst = pathlib.Path(dst)
+    _log.info('writing %s file %s', format, dst)
+    if format == 'pickle':
+        with dst.open('wb') as f:
+            return pickle.dump(obj, f, protocol=protocol)
+    elif format == 'binpickle':
+        binpickle.dump(obj, dst)
+    else:
+        _log.error('invalid destination format %s', format)
+        raise ValueError('invalid destination format ' + format)
+
+
+def main(opts):
+    obj = read_object(opts['SRC'], opts['-f'])
+    write_object(obj, opts['DST'], opts['-t'], opts['-p'])
+
+
+if __name__ == '__main__':
+    opts = docopt(__doc__)
+    level = logging.DEBUG if opts['--verbose'] else logging.INFO
+    logging.basicConfig(level=level)
---
+++ tests/test_repickle.py
@@ -0,0 +1,66 @@
+import pytest
+try:
+    from docopt import docopt
+    from binpickle import repickle
+except ImportError:
+    pytestmark = pytest.mark.skip('repickle deps not available')
+
+import binpickle
+import pickle
+
+import numpy as np
+import pandas as pd
+
+from hypothesis import given, assume
+import hypothesis.strategies as st
+import hypothesis.extra.numpy as nph
+
+def do_repickle(*args):
+    opts = docopt(repickle.__doc__, argv=[str(s) for s in args])
+    repickle.main(opts)
+
+
+@given(st.data())
+def test_pickle_to_binpickle(tmp_path, data):
+    n = data.draw(st.integers(1, 10000))
+    df = pd.DataFrame({
+        'key': np.arange(0, n),
+        'count': data.draw(nph.arrays(np.int32, n)),
+        'score': data.draw(nph.arrays(np.float64, n))
+    })
+    assume(not df['score'].isna().any())
+
+    src = tmp_path / 'df.pkl'
+    dst = tmp_path / 'df.bpk'
+    df.to_pickle(src)
+    do_repickle('-f', 'pickle', '-t', 'binpickle', src, dst)
+
+    assert dst.exists()
+    df2 = binpickle.load(dst)
+
+    assert all(df2['key'] == df['key'])
+    assert all(df2['count'] == df['count'])
+    assert all(df2['score'] == df['score'])
+
+
+@given(st.data())
+def test_binpickle_to_pickle(tmp_path, data):
+    n = data.draw(st.integers(1, 10000))
+    df = pd.DataFrame({
+        'key': np.arange(0, n),
+        'count': data.draw(nph.arrays(np.int32, n)),
+        'score': data.draw(nph.arrays(np.float64, n))
+    })
+    assume(not df['score'].isna().any())
+
+    src = tmp_path / 'df.bpk'
+    dst = tmp_path / 'df.pkl'
+    binpickle.dump(df, src)
+    do_repickle('-f', 'binpickle', '-t', 'pickle', src, dst)
+
+    assert dst.exists()
+    df2 = pd.read_pickle(dst)
+
+    assert all(df2['key'] == df['key'])
+    assert all(df2['count'] == df['count'])
+    assert all(df2['score'] == df['score'])
