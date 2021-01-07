ee5c0d725ed2d6e1560292086b32861d97f6a697
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-01-07 12:58:46 -0700

    Bump package version

--- binpickle/__init__.py
+++ binpickle/__init__.py
@@ -2,7 +2,7 @@
 Optimized format for pickling binary data.
 """
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
 from .write import dump, BinPickler    # noqa: F401
 from .read import load, BinPickleFile  # noqa: F401
