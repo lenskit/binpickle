684e8575a8eede58391e11ae6cce9c4f46b49363
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-01-30 11:31:04 -0700

    next binpickle version

--- binpickle/__init__.py
+++ binpickle/__init__.py
@@ -2,7 +2,7 @@
 Optimized format for pickling binary data.
 """
 
-__version__ = '0.3.3'
+__version__ = '0.4.0'
 
 from .write import dump, BinPickler    # noqa: F401
 from .read import load, BinPickleFile  # noqa: F401
