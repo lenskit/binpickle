e7afc178ac903d729a09b2b953eb23963e4c6245
Michael Ekstrand <md@ekstrandom.net>
2022-01-22 13:18:44 -0700

    Bump version

--- binpickle/__init__.py
+++ binpickle/__init__.py
@@ -2,7 +2,7 @@
 Optimized format for pickling binary data.
 """
 
-__version__ = '0.3.4'
+__version__ = '0.4.0'
 
 from .write import dump, BinPickler    # noqa: F401
 from .read import load, BinPickleFile  # noqa: F401
