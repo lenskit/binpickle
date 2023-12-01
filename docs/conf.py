bbb0b82ade7cf7488af672d2a219438cc0605ec7
Michael Ekstrand <mdekstrand@drexel.edu>
2023-12-01 17:34:23 -0500

    bump copier

--- docs/conf.py
+++ docs/conf.py
@@ -1,44 +1,40 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
 import binpickle
 
-project = 'BinPickle'
-copyright = '2020-2022 Boise State University'
-author = 'Michael D. Ekstrand'
+project = "BinPickle""
+copyright = "2023 Michael Ekstrand"
+author = "Michael D. Ekstrand"
 
 release = binpickle.__version__
 
 extensions = [
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinxext.opengraph",
 ]
 
-source_suffix = '.rst'
+source_suffix = ".rst"
 
-pygments_style = 'sphinx'
-highlight_language = 'python3'
+pygments_style = "sphinx"
+highlight_language = "python3"
 
-html_theme = 'furo'
+html_theme = "furo"
 html_theme_options = {
-    'github_user': 'lenskit',
-    'github_repo': 'binpickle',
-    'travis_button': False,
-    # 'canonical_url': 'https://binpickle.lenskit.org/',
-    # 'font_family': 'Georgia, Charter, serif'
 }
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'numba': ('https://numba.readthedocs.io/en/stable/', None),
-    'numcodecs': ('https://numcodecs.readthedocs.io/en/stable/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
+    "sklearn": ("https://scikit-learn.org/stable/", None),
 }
 
 autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource'
+    "members": True,
+    "member-order": "bysource"
 }
